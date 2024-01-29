18a3a6fb8ef04c1eee34193849da1b5aa8ab5ee5
jenaamj <jenaamj@gmail.com>
2024-01-30 01:15:44 +0530

    path add

--- index.js
+++ index.js
@@ -2,26 +2,35 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import Dotenv from "dotenv";
-import dbConnect  from "./db/db.js";
+import dbConnect from "./db/db.js";
+import path from "path";
+import { fileURLToPath } from 'url'; // Import fileURLToPath function
+
+const __filename = fileURLToPath(import.meta.url); // Get the current module's file path
+const __dirname = path.dirname(__filename); // Derive the directory path
+
 const app = express();
 Dotenv.config();
 
 app.use(bodyParser.json());
-
 app.use(cors());
 
-// Routes 
-
+// Routes
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/user.js'
 import questionRoutes from './routes/question.js'
 import quizRoutes from './routes/quiz.js'
 
-app.use("/api",authRoutes)
-app.use("/api",userRoutes)
-app.use("/api",questionRoutes)
-app.use("/api",quizRoutes)
+app.use("/api", authRoutes)
+app.use("/api", userRoutes)
+app.use("/api", questionRoutes)
+app.use("/api", quizRoutes)
 
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
 
 dbConnect();
 
